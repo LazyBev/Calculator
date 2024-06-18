768d4021b2227ff0ac33538ad26cb27b405f8e83
Lazy Beverage <157259616+LazyBev@users.noreply.github.com>
2024-06-18 05:32:28 +0000

    Update calc.c

--- C/calc.c
+++ C/calc.c
@@ -8,41 +8,63 @@ typedef union {
     float f;
 } FloatIntUnion;
 
-#define PI 3.14159265358979323846
+#define PI 3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679
 #define NAN (float)(FloatIntUnion){ .i = 0x7FC00000 }.f
 
 float my_fabs(float x, float y);
-float my_factorial(float x, int y);
+float my_log(float x, float n);
+float my_fact(float x, int y);
 float my_pow(float x, int y);
 float my_sqrt(float x, float y);
+float my_nrt(float x, float y);
 float my_sin(float x, float y);
 float my_asin(float x, float y);
 float calculator(char* op, float x, float y);
 
-float my_fabs(float x, float y) {
-    float result = y < 0 ? -y : y;
+float my_fabs(float x, float y) { // It worky :D
+    return (y < 0 ? -y : y) * x;
+}
+
+float my_abs(float x, float y) { // It worky :D
+    return (int)(y < 0 ? -y : y) * x;
+}
+
+float my_fact(float x, int y) { // It worky :D
+    float result = y;
+    result = (y == 0) ? 1.0 : y;
+    result = (y != 1) ? result : 1;
+    for (int i = (y - 1); i != 1; --i) { result *= i; } 
+
     return result *= x;
 }
 
-float my_factorial(float x, int y) {
-    if (y == 0 || y == 1) {
-        return 1.0;
-    }
-    float result = 1.0;
-    for (int i = 2; i <= y; i++) {
-        result *= i;
+float my_log(float x, float y) { // It worky :D
+    if (y <= 0.0) {
+        fprintf(stderr, "Error: Logarithm is only defined for x > 0.\n");
+        return -1.0;
     }
 
-    result *= x;
-    return result;
+    float result = 0.0;
+    float delta = (y - 1.0) / (y + 1.0);
+    float delta_squared = delta * delta;
+    float numerator = delta;
+    int n = 1;
+
+    while (my_pow(delta, n) / n >= 1e-6) {
+        result += 2.0 * numerator / n;
+        numerator *= delta_squared;
+        n += 2;
+    }
+
+    return result *= x;
 }
 
-float my_pow(float x, int y) {
+float my_pow(float x, int y) { // It worky :D
     float result = 1.0;
     if (y == 0) {
-        return 1.0;
+        result = 1.0;
     } else if (y < 0) {
-        return 1.0 / my_pow(x, -y);
+        result = 1.0 / my_pow(x, -y);
     }
     for (int i = 0; i < y; i++) {
         result *= x;
@@ -50,7 +72,24 @@ float my_pow(float x, int y) {
     return result;
 }
 
-float my_sqrt(float x, float y) {
+float nrt(float x, float y) { // It worky :D
+    if (x <= 0.0) {
+        fprintf(stderr, "Error: x must be greater than 0.\n");
+        return -1.0;
+    }
+
+    float guess = y / x;
+    float epsilon = 1e-6;
+
+    while (my_pow(guess, x) - y > epsilon || y - my_pow(guess, x) > epsilon) {
+        guess = guess - (my_pow(guess, x) - y) / (x * my_pow(guess, x - 1));
+    }
+
+    return guess;
+}
+
+
+float my_sqrt(float x, float y) { // It worky :D
     if (y < 0.0) {
         fprintf(stderr, "Error: Square root of negative number.\n");
         return NAN;
@@ -74,16 +113,15 @@ float my_sqrt(float x, float y) {
 float my_sin(float x, float y) { // is completely brocken :'C
     float result = y;
     int sign = -1;
-    float term = y - sign * my_pow(y, 3) / my_factorial(1, 3);;
+    float term = y - sign * my_pow(y, 3) / my_fact(1, 3);;
 
     for (int i = 5; my_fabs(1, term) > 1e-100; i += 2) {
-        term = sign * my_pow(y, i) / my_factorial(1, i);
+        term = sign * my_pow(y, i) / my_fact(1, i);
         result += term;
         sign *= -1;
     }
-
-    result *= x; 
-    return result;
+ 
+    return result *= x;
 }
 
 float my_asin(float x, float y) { // is slightly brocken :'C
@@ -103,12 +141,10 @@ float my_asin(float x, float y) { // is slightly brocken :'C
         n++;
     }
 
-    result *= x;
-
-    return result;
+    return result *= x;
 }
 
-float calculator(char* op, float x, float y) {
+float calculator(char* op, float x, float y) { // It worky :D
     system("clear");
     float result = 0.0;
 
@@ -121,9 +157,13 @@ float calculator(char* op, float x, float y) {
     } else if (strcmp(op, "/") == 0) {
         result = (y == 0.0) ? NAN : x / y;
     } else if (strcmp(op, "!") == 0) {
-        result = my_factorial(x, (int)y);
+        result = my_fact(x, (int)y);
     } else if (strcmp(op, "sq") == 0) {
         result = x * (y * y);
+    } else if (strcmp(op, "nrt") == 0) {
+        result = nrt(x, y);
+    } else if (strcmp(op, "log") == 0) {
+        result = my_log(x, y);
     } else if (strcmp(op, "sqrt") == 0) {
         result = my_sqrt(x, y);
     } else if (strcmp(op, "pow") == 0) {
@@ -132,6 +172,10 @@ float calculator(char* op, float x, float y) {
         result = my_sin(x, y);
     } else if (strcmp(op, "asin") == 0) {
         result = my_asin(x, y);
+    } else if (strcmp(op, "fabs") == 0){
+        result = my_fabs(x, y);
+    } else if (strcmp(op, "abs") == 0){
+        result = my_abs(x, y);
     } else {
         printf("Unknown operation.\n");
         result = NAN;
@@ -142,9 +186,10 @@ float calculator(char* op, float x, float y) {
     return result;
 }
 
-int main() {
+int main() { // It worky :D
     system("clear");
-    char *op = (char *)malloc(6 * sizeof(char)); 
+    char *op = (char *)malloc(6 * sizeof(char));
+    float x, y;
     int yn;
 
     printf("How many variables: ");
@@ -152,11 +197,11 @@ int main() {
 
     system("clear");
     if (yn == 2) {
-        printf("(op x y): ");
+        printf("(format: op x y): ");
         scanf("%s %f %f", op, &x, &y);
         calculator(op, x, y);
     } else {
-        printf("(op x): ");
+        printf("(format: op x): ");
         scanf("%s %f", op, &y);
         calculator(op, 1, y);
     }
